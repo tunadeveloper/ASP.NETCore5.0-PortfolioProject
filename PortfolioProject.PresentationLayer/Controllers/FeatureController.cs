3c56e67ddae26b205d6c03e0a2265d955fb228eb
tunadeveloper <tunabusiness25@gmail.com>
2025-05-07 21:01:55 +0300

    updated

---
+++ PortfolioProject.PresentationLayer/Controllers/FeatureController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PortfolioProject.PresentationLayer.Controllers
+{
+    public class FeatureController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ PortfolioProject.PresentationLayer/Controllers/SkillController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PortfolioProject.PresentationLayer.Controllers
+{
+    public class SkillController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
