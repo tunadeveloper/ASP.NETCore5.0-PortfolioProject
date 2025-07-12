72977da13128c7c4458f6333fffc782faa4e505a
tunadeveloper <tunabusiness25@gmail.com>
2025-07-12 23:15:04 +0300

    updated

---
+++ PortfolioProject.PresentationLayer/Areas/Writer/Controllers/DashboardController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using PortfolioProject.DataAccessLayer.Concrete;
+using PortfolioProject.EntityLayer.Concrete;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace PortfolioProject.PresentationLayer.Areas.Writer.Controllers
+{
+    [Area("Writer")]
+    public class DashboardController : Controller
+    {
+        private readonly UserManager<WriterUser> _userManager;
+
+        public DashboardController(UserManager<WriterUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var values = await _userManager.FindByNameAsync(User.Identity.Name);
+            ViewBag.v = values.Name + " " + values.Surname;
+
+            string api = "799df0672eaf0c89c2e03413273a862a";
+            string conntection = "http://api.openweathermap.org/data/2.5/weather?q=erzurum&mode=xml&lang=tr&units=metric&appid=" + api;
+            XDocument document = XDocument.Load(conntection);
+            ViewBag.v5 = document.Descendants("temperature").ElementAt(0).Attribute("value").Value;
+
+            Context context = new Context();
+            ViewBag.v1 = 0;
+            ViewBag.v2 = context.Announcements.Count();
+            ViewBag.v3 = 0;
+            ViewBag.v4 = context.Skills.Count();
+            return View();
+        }
+    }
+}
---
+++ PortfolioProject.PresentationLayer/Areas/Writer/Controllers/ProfileController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using PortfolioProject.EntityLayer.Concrete;
+using PortfolioProject.PresentationLayer.Areas.Writer.Models;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace PortfolioProject.PresentationLayer.Areas.Writer.Controllers
+{
+    [Area("Writer")]
+    public class ProfileController : Controller
+    {
+        private readonly UserManager<WriterUser> _userManager;
+
+        public ProfileController(UserManager<WriterUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var values = await _userManager.FindByNameAsync(User.Identity.Name);
+            UserEditViewModel model = new UserEditViewModel();
+            model.Name = values.Name;
+            model.Surname = values.Surname;
+            model.PictureURL = values.ImageUrl;
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(UserEditViewModel u)
+        {
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+            if (u.Picture != null)
+            {
+                var resource = Directory.GetCurrentDirectory();
+                var extension = Path.GetExtension(u.Picture.FileName);
+                var imageName = Guid.NewGuid() + extension;
+                var saveLocation = resource + "/wwwroot/UserImage/" + imageName;
+                var stream = new FileStream(saveLocation, FileMode.Create);
+                await u.Picture.CopyToAsync(stream);
+               user.ImageUrl = imageName;
+            }
+            user.Name = u.Name;
+            user.Surname = u.Surname;
+            var result = await _userManager.UpdateAsync(user);
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index", "Default", "Writer");
+            }
+            return View();
+        }
+    }
+}
